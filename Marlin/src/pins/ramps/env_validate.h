09c381b7bf5853e2d9efbdda7b2edb233b961341
jlachesk <jlachesk@gmail.com>
2024-12-27 17:46:32 -0500

    env_validate hax

--- Marlin/src/pins/ramps/env_validate.h
+++ Marlin/src/pins/ramps/env_validate.h
@@ -26,10 +26,10 @@
   #if NOT_TARGET(__SAM3X8E__, __AVR_ATmega2560__)
     #error "Oops! Select 'Arduino Due' or 'Arduino/Genuino Mega or Mega 2560' in 'Tools > Board.'"
   #endif
-#elif ENABLED(REQUIRE_MEGA2560) && NOT_TARGET(__AVR_ATmega2560__)
-  #error "Oops! Select 'Arduino/Genuino Mega or Mega 2560' in 'Tools > Board.'"
-#elif DISABLED(REQUIRE_MEGA2560) && NOT_TARGET(__AVR_ATmega1280__, __AVR_ATmega2560__)
-  #error "Oops! Select 'Arduino/Genuino Mega or Mega 2560 or 1280' in 'Tools > Board.'"
+//#elif ENABLED(REQUIRE_MEGA2560) && NOT_TARGET(__AVR_ATmega2560__)
+//  #error "Oops! Select 'Arduino/Genuino Mega or Mega 2560' in 'Tools > Board.'"
+//#elif DISABLED(REQUIRE_MEGA2560) && NOT_TARGET(__AVR_ATmega1280__, __AVR_ATmega2560__)
+//  #error "Oops! Select 'Arduino/Genuino Mega or Mega 2560 or 1280' in 'Tools > Board.'"
 #endif
 
 #undef ALLOW_SAM3X8E
